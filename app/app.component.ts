5ded72b395e802a8634a641aa9184fdbc5fd583e
Milton Li <tropa9527@gmail.com>
2017-01-07 23:22:16 +0800

    Added a reusable service to return heroes

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  providers: [HeroService],
+  styleUrls: ['app/app.component.css'],
+  template:
+  `
+    <h1>{{title}}</h1>
+    <ul class="heroes">
+      <li *ngFor="let hero of heroes"
+          [class.selected]="selectedHero === hero"
+          (click)="onSelect(hero)">
+        <span class="badge">{{hero.id}}</span> {{hero.name}}
+      </li>
+    </ul>
+    <my-hero-detail [hero]="selectedHero"></my-hero-detail>
+  `
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent implements OnInit  {
+  title = 'Tour of Heroes';
+  selectedHero: Hero;
+  heroes: Hero[];
+
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+}
